3d09dae0e2d0075c1033814a7e4ec23293d82d21
SandraMeshack <56250836+SandraMeshack@users.noreply.github.com>
2020-06-20 02:32:05 +0100

    Same code for my thesis
    This model uses the same code for my thesis, a little bit of twaeking with the codes. However, the major change is the use of the FER2013 dataset instead of the jaffe dataset.

---
+++ Train.py
@@ -0,0 +1,106 @@
+from pyexpat import model
+
+import keras
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, BatchNormalization, Conv2D, MaxPooling2D
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+classes = 7   #this is the amount of facial expressions
+rows = 48    #dimensions of images
+col = 48    #dimensions of images
+size = 8    #number of images to train at a time
+training_samples = 28709 #number of samples available for training
+test_samples = 3589 #number of samples available for validation
+epochs = 100 #number of epochs
+##Prepare training and validation sets
+data_training = '/home/chime/PycharmProjects/Testthesis/fer2013/images_fer2013/Training' #path of training data
+data_testing = '/home/chime/PycharmProjects/Testthesis/fer2013/images_fer2013/PrivateTest'   #path of test data
+
+##reshape and rescale the images to get more images from the images already present for better accuracy(Data augmentation)
+data_training_generation = ImageDataGenerator(rescale=1./255, rotation_range=40,
+                                              shear_range=0.2,zoom_range=0.2, width_shift_range=0.2,
+                                              height_shift_range=0.2, horizontal_flip=True, vertical_flip=True, fill_mode='nearest')
+data_testing_generation = ImageDataGenerator(rescale=1./255)
+##generate the images to be used from the path and the reshaping done for  training and testing and send to the NN
+data_training_generator = data_training_generation.flow_from_directory(data_training, color_mode='grayscale', target_size=(rows, col),
+                                                                       batch_size=size, class_mode='categorical', shuffle=True)
+
+data_testing_generator = data_testing_generation.flow_from_directory(data_testing, color_mode='grayscale',
+                                                                     target_size=(rows, col), batch_size=size, class_mode='categorical', shuffle=True)
+
+#CNN training
+##Define model
+model = Sequential()
+##FIRST SET OF LAYERS
+model.add(Conv2D(filters= 32,kernel_size= (3,3), padding='same', kernel_initializer='he_normal',
+                 input_shape=(rows, col, 1), activation='elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(filters=32,kernel_size= (3,3), padding='same', kernel_initializer='he_normal',
+                 input_shape=(rows, col, 1), activation='elu'))
+model.add(BatchNormalization())
+##POOLING LAYER
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+
+##SECOND SET OF LAYERS
+model.add(Conv2D(filters=64,kernel_size=(3,3), padding='same', kernel_initializer='he_normal', activation='elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(filters=64,kernel_size=(3,3), padding='same', kernel_initializer='he_normal', activation='elu'))
+model.add(BatchNormalization())
+##POOLING LAYER
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+
+##THIRD SET OF LAYERS
+
+model.add(Conv2D(filters= 128,kernel_size=(3,3), padding='same', kernel_initializer='he_normal', activation='elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(filters=128,kernel_size=(3,3), padding='same', kernel_initializer='he_normal', activation= 'elu'))
+model.add(BatchNormalization())
+##POOLING LAYER
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
+
+## FOURTH SET OF LAYERS
+
+model.add(Conv2D(filters=256,kernel_size=(3,3), padding='same', kernel_initializer='he_normal', activation='elu'))
+#model.add(BatchNormalization())
+model.add(Conv2D(filters=256,kernel_size=(3,3), padding='same', kernel_initializer='he_normal',activation='elu'))
+model.add(BatchNormalization())
+##POOLING LAYER
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
+
+#FLATTEN  IMAGES
+model.add(Flatten())
+model.add(Dense(64, kernel_initializer= 'he_normal', activation= 'elu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+
+#SIXTH LAYER
+model.add(Dense(64, kernel_initializer='he_normal', activation= 'elu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+
+#SEVENTH LAYER(CLASSIFIER LAYER)
+model.add(Dense(classes, kernel_initializer='he_normal', activation= 'softmax'))
+
+
+print(model.summary())
+##Callback functions
+##modelcheckpoint to save the model with the best performance
+check = ModelCheckpoint('/home/chime/PycharmProjects/Testthesis/Emotions.h4',monitor='val_loss', mode='min', save_best_only=True, verbose=1)
+## Stop training when performance measure stops improving after 5 times i.e patience
+stop = EarlyStopping(monitor='val_loss', mode='min', min_delta=0, patience=80, verbose=1, restore_best_weights=True)
+
+##monitor validation loss and reduce learning rate if validation loss does not improve for 5 epochs which means you are learning slowly
+learning_rate = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=80, verbose=1, min_delta=0.0001)
+##Callback function to execute code
+model_call_back = [check, stop, learning_rate]
+##Compile the model
+model.compile(loss='categorical_crossentropy', optimizer= Adam(learning_rate=0.001), metrics=['accuracy'])
+#fit model
+history = model.fit_generator(data_training_generator, steps_per_epoch=training_samples//size,
+                              epochs= epochs, callbacks= model_call_back, validation_data= data_testing_generator,
+                              validation_steps= test_samples//size)
---
+++ test.py
@@ -0,0 +1,53 @@
+from keras.models import load_model
+from keras.preprocessing.image import img_to_array
+import cv2
+import numpy as np
+
+##import cascade library(downloaded from udemy course on python and machine learning)
+cascade_classifier = cv2.CascadeClassifier('/home/chime/PycharmProjects/Testthesis/haarcascade_frontalface_default.xml')
+## defined a model saved from the training session
+model = load_model('/home/chime/PycharmProjects/Testthesis/Emotions.h5')
+## defining the labels of emotions available
+classes = ['anger', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprised']
+# initilaising camera(laptop camera= 0, external=1)
+cap = cv2.VideoCapture(0)
+
+while True:
+    ## return the frame being seen by the camera
+    ret, face = cap.read()
+    labels = []
+    ##convert bgr to gray for easy comparism with model
+    bgr2gray = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+    ##Reduce the frame of the of the image and detect features using knn
+    face_rects = cascade_classifier.detectMultiScale(bgr2gray,1.3,5)
+
+    for (x,y,w,h) in face_rects:
+        ##white solid rectangle for facemapping and thickness of 10
+        cv2.rectangle(face, (x, y), (x+w, y+h), (255, 255, 255), 10)
+        gray_roi = bgr2gray[y:y+h, x:x+w]
+        gray_roi = cv2.resize(gray_roi, (48, 48), interpolation=cv2.INTER_AREA)
+
+
+
+        if np.sum([gray_roi])!=0:
+            roi = gray_roi.astype('float')/255.0
+            roi = img_to_array(roi)
+            roi = np.expand_dims(roi, axis=0)
+
+        # fetch from the classes and predict the microexpression
+
+            prediction = model.predict(roi)[0]
+            microexpression = classes[prediction.argmax()]
+            label_position = (x,y)
+            ##Text color and type for the microexpression
+            cv2.putText(face, microexpression, label_position, cv2.FONT_HERSHEY_COMPLEX_SMALL, 3, (0, 0, 255), 5)
+        else:
+            ##Text color and type if no face to detect microexpression
+            cv2.putText(face, 'No Face Found', (20, 60), cv2.FONT_HERSHEY_COMPLEX_SMALL, 3, (0, 255, 0), 2)
+    cv2.imshow('Micro-expression', face)
+    ##quit when q is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
