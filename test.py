af28b6948619ad42561ac20d1e05ae6eea2bc38d
SandraMeshack <56250836+SandraMeshack@users.noreply.github.com>
2020-07-22 13:07:10 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -11,14 +11,22 @@ model = load_model('/home/chime/PycharmProjects/Testthesis/Emotions.h5')
 classes = ['anger', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprised']
 # initilaising camera(laptop camera= 0, external=1)
 #cap = cv2.VideoCapture(0)
-cap = cv2.VideoCapture('/home/chime/PycharmProjects/Testthesis/frankietruthh.mp4')
+#cap = cv2.VideoCapture('/home/chime/PycharmProjects/Testthesis/frankie2.mp4')
+cap = cv2.VideoCapture('/home/chime/PycharmProjects/Testthesis/clinton.mp4')
 while True:
     ## return the frame being seen by the camera
     ret, face = cap.read()
     labels = []
+    angry_0 = []
+    disgust_1 = []
+    fear_2 = []
+    happy_3 = []
+    neutral_4 = []
+    sad_5 = []
+    surprise_6 = []
     ##convert bgr to gray for easy comparism with model
     bgr2gray = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
-    ##Reduce the frame of the of the image and detect features using knn
+    ##Reduce the frame of the of the image and detect features
     face_rects = cascade_classifier.detectMultiScale(bgr2gray,1.3,5)
 
     for (x,y,w,h) in face_rects:
@@ -37,10 +45,34 @@ while True:
         # fetch from the classes and predict the microexpression
 
             prediction = model.predict(roi)[0]
+            angry_0.append(prediction[0].astype(float))
+            disgust_1.append(prediction[1].astype(float))
+            fear_2.append(prediction[2].astype(float))
+            happy_3.append(prediction[3].astype(float))
+            neutral_4.append(prediction[4].astype(float))
+            sad_5.append(prediction[5].astype(float))
+            surprise_6.append(prediction[6].astype(float))
             microexpression = classes[prediction.argmax()]
             label_position = (x,y)
+            i=0
             ##Text color and type for the microexpression
-            cv2.putText(face, microexpression, label_position, cv2.FONT_HERSHEY_COMPLEX_SMALL, 3, (0, 0, 255), 5)
+            cv2.putText(face, microexpression, label_position, cv2.FONT_HERSHEY_COMPLEX_SMALL, 2.5, (0, 0, 255),2)
+            cv2.putText(face, "Angry : " + str(np.round(prediction[0]*100, 2)) + "%", (40, 140 + 180*i),
+                        cv2.FONT_HERSHEY_COMPLEX, 0.5, 255, 1)
+            cv2.putText(face, "Disgust : " + str(np.round(prediction[1]*100, 2)) + "%", (40, 160 + 180*i ),
+                        cv2.FONT_HERSHEY_COMPLEX, 0.5, 255, 1)
+            cv2.putText(face, "Fear : " + str(np.round(prediction[2]*100, 2)) + "%", (40,180 + 180*i ),
+                        cv2.FONT_HERSHEY_COMPLEX, 0.5, 255, 1)
+            cv2.putText(face, "Happy : " + str(np.round(prediction[3]*100, 2)) + "%", (40, 200 + 180 *i),
+                        cv2.FONT_HERSHEY_COMPLEX, 0.5, 255, 1)
+            cv2.putText(face, "Neutral : " + str(np.round(prediction[4]*100, 2)) + "%", (40, 220 + 180 *i),
+                        cv2.FONT_HERSHEY_COMPLEX, 0.5, 255, 1)
+            cv2.putText(face, "Sad : " + str(np.round(prediction[5]*100, 2)) + "%", (40, 240 + 180 *i),
+                        cv2.FONT_HERSHEY_COMPLEX, 0.5, 255, 1)
+            cv2.putText(face, "Surprise : " + str(np.round(prediction[6]*100, 2)) + "%", (40, 260 + 180 *i),
+                        cv2.FONT_HERSHEY_COMPLEX, 0.5, 255, 1)
+
+
         else:
             ##Text color and type if no face to detect microexpression
             cv2.putText(face, 'No Face Found', (20, 60), cv2.FONT_HERSHEY_COMPLEX_SMALL, 3, (0, 255, 0), 2)
