49657ef41655ca4ad6e9018b8c1ef30da639b14f
SandraMeshack <56250836+SandraMeshack@users.noreply.github.com>
2020-08-14 23:03:36 +0100

    Final upgrade on code
    final upgrade on thesis code

--- Train.py
+++ Train.py
@@ -8,34 +8,49 @@ from sklearn.metrics import confusion_matrix,classification_report
 classes = 7   #this is the amount of facial expressions
 rows = 48    #dimensions of images
 col = 48    #dimensions of images
-size = 32   #number of images to train at a time
+size = 32 #number of images to train at a time
 training_samples = 28709 #number of samples available for training
 test_samples = 3589 #number of samples available for validation
-epochs = 500 #number of epochs
-val = test_samples//size
+epochs = 1 #number of epochs
+val = test_samples//size #validation steps
+stepsepochs=training_samples//size #number of steps per epoch
 ##Prepare training and validation sets
 data_training = '/home/chime/PycharmProjects/Testthesis/fer2013/images_fer2013/Training' #path of training data
 data_testing = '/home/chime/PycharmProjects/Testthesis/fer2013/images_fer2013/PrivateTest'   #path of test data
 ##reshape and rescale the images to get more images from the images already present for better accuracy(Data augmentation)
+
 data_training_generation = ImageDataGenerator(rescale=1./255, rotation_range=40,
-                                              shear_range=0.2,zoom_range=0.2, width_shift_range=0.2,
-                                              height_shift_range=0.2, horizontal_flip=True, vertical_flip=True, fill_mode='nearest')
+                                              shear_range=0.2,zoom_range=0.2, zca_whitening=False,width_shift_range=0.3,
+                                              height_shift_range=0.3,data_format=None,validation_split=0.0,
+                                              horizontal_flip=True, vertical_flip=True, brightness_range=None,
+                                              fill_mode='nearest', preprocessing_function=None)
+
 data_testing_generation = ImageDataGenerator(rescale=1./255)
 ##generate the images to be used from the path and the reshaping done for  training and testing and send to the NN
-data_training_generator = data_training_generation.flow_from_directory(data_training, color_mode='grayscale', target_size=(rows, col),
-                                                                       batch_size=size, class_mode='categorical', shuffle=True)
+##color_mode is grayscale because the images are in grayscale
+##batchsize is the numer of images to be used from the generator per batch and this can be changed
+##class mode is categorical because we have about 7 classes
+##shuffle is true because we intend on shuffling the order of the yielded images
+##seed is for applying random image augmentation
+data_training_generator = data_training_generation.flow_from_directory(data_training, target_size=(rows, col),color_mode='grayscale',
+                                                                        batch_size=size,class_mode='categorical', shuffle=True,seed=42,
+                                                                         follow_links=False,subset=None)
 
-data_testing_generator = data_testing_generation.flow_from_directory(data_testing, color_mode='grayscale',
-                                                                     target_size=(rows, col), batch_size=size, class_mode='categorical', shuffle=True)
+data_testing_generator = data_testing_generation.flow_from_directory(data_testing, target_size=(rows, col),color_mode='grayscale',
+                                                                      batch_size=size, class_mode='categorical', shuffle=True,seed=42,
+                                                                     follow_links=False, subset=None)
 
 #CNN training
 ##Define model
 model = Sequential()
 ##FIRST SET OF LAYERS
-model.add(Conv2D(filters= 32,kernel_size= (3,3), padding='same', kernel_initializer='he_normal',
+## The code for the convolutional layer is a universal code for using relu as an activation function
+## Most develops add one after the other for conv2d but i prefer adding everything to the model at once to make it more readable
+## strides can be ignored as default is (1,1) but i'd rather use it to ensure the code is running at optimum
+model.add(Conv2D(filters= 32,kernel_size= (3,3), strides=(1,1),padding='same', kernel_initializer='he_normal',
                  input_shape=(rows, col, 1), activation='relu'))
 model.add(BatchNormalization())
-model.add(Conv2D(filters=32,kernel_size= (3,3), padding='same', kernel_initializer='he_normal',
+model.add(Conv2D(filters=32,kernel_size= (3,3), strides=(1,1),padding='same', kernel_initializer='he_normal',
                  input_shape=(rows, col, 1), activation='relu'))
 model.add(BatchNormalization())
 ##POOLING LAYER
@@ -43,9 +58,9 @@ model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.2))
 
 ##SECOND SET OF LAYERS
-model.add(Conv2D(filters=64,kernel_size=(3,3), padding='same', kernel_initializer='he_normal', activation='relu'))
+model.add(Conv2D(filters=64,kernel_size=(3,3),strides=(1,1), padding='same', kernel_initializer='he_normal', activation='relu'))
 model.add(BatchNormalization())
-model.add(Conv2D(filters=64,kernel_size=(3,3), padding='same', kernel_initializer='he_normal', activation='relu'))
+model.add(Conv2D(filters=64,kernel_size=(3,3), strides=(1,1),padding='same', kernel_initializer='he_normal', activation='relu'))
 model.add(BatchNormalization())
 ##POOLING LAYER
 model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -53,9 +68,9 @@ model.add(Dropout(0.2))
 
 ##THIRD SET OF LAYERS
 
-model.add(Conv2D(filters= 128,kernel_size=(3,3), padding='same', kernel_initializer='he_normal', activation='relu'))
+model.add(Conv2D(filters= 128,kernel_size=(3,3), strides=(1,1),padding='same', kernel_initializer='he_normal', activation='relu'))
 model.add(BatchNormalization())
-model.add(Conv2D(filters=128,kernel_size=(3,3), padding='same', kernel_initializer='he_normal', activation= 'relu'))
+model.add(Conv2D(filters=128,kernel_size=(3,3),strides=(1,1), padding='same', kernel_initializer='he_normal', activation= 'relu'))
 model.add(BatchNormalization())
 ##POOLING LAYER
 model.add(MaxPooling2D(pool_size=(2,2)))
@@ -63,9 +78,18 @@ model.add(Dropout(0.2))
 
 ## FOURTH SET OF LAYERS
 
-model.add(Conv2D(filters=256,kernel_size=(3,3), padding='same', kernel_initializer='he_normal', activation='relu'))
+model.add(Conv2D(filters=256,kernel_size=(3,3),strides=(1,1), padding='same', kernel_initializer='he_normal', activation='relu'))
 model.add(BatchNormalization())
-model.add(Conv2D(filters=256,kernel_size=(3,3), padding='same', kernel_initializer='he_normal',activation='relu'))
+model.add(Conv2D(filters=256,kernel_size=(3,3),strides=(1,1), padding='same', kernel_initializer='he_normal',activation='relu'))
+model.add(BatchNormalization())
+##POOLING LAYER
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
+## FIFTH SET OF LAYERS
+
+model.add(Conv2D(filters=512,kernel_size=(3,3),strides=(1,1),padding='same', kernel_initializer='he_normal', activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(filters=512,kernel_size=(3,3), strides=(1,1),padding='same', kernel_initializer='he_normal',activation='relu'))
 model.add(BatchNormalization())
 ##POOLING LAYER
 model.add(MaxPooling2D(pool_size=(2,2)))
@@ -87,22 +111,32 @@ model.add(Dense(classes, kernel_initializer='he_normal', activation= 'softmax'))
 
 
 print(model.summary())
+
+
 ##Callback functions
 ##modelcheckpoint to save the model with the best performance
-check = ModelCheckpoint('/home/chime/PycharmProjects/Testthesis/Emotions.h5',monitor='val_loss', mode='min', save_best_only=True, verbose=1)
-## Stop training when performance measure stops improving after 5 times i.e patience
-stop = EarlyStopping(monitor='val_loss', mode='min', min_delta=0, patience=400, verbose=1, restore_best_weights=True)
+##For this model we are monitoring the validation accuracy and saving the maximum value of the validation accuracy
+check = ModelCheckpoint(filepath='/home/chime/PycharmProjects/Testthesis/Emotions.h5',monitor='val_accuracy', verbose=1 ,save_best_only=True, mode='max')
+## Stop training when performance measure stops improving after patience times i.e patience
+##The measure being monitored is the validation loss
+##min_delta is the minimum chnage thet can be considered an improvement
+stop = EarlyStopping(monitor='val_loss', mode='min', min_delta=0, patience=2400, verbose=1, baseline=None,restore_best_weights=True)
 
-##monitor validation loss and reduce learning rate if validation loss does not improve for 5 epochs which means you are learning slowly
-learning_rate = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=400, verbose=1, min_delta=0.0001)
+
+##monitor validation loss and reduce learning rate if validation loss does not improve for patience  which means you are learning slowly
+
+learning_rate = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=2400, verbose=1, min_delta=0.0001)
+
+
 ##Callback function to execute code
 model_call_back = [check, stop, learning_rate]
 ##Compile the model
 model.compile(loss='categorical_crossentropy', optimizer= Adam(learning_rate=0.001), metrics=['accuracy'])
 #fit model
-history = model.fit_generator(data_training_generator, steps_per_epoch=training_samples//size,
+history = model.fit_generator(data_training_generator, steps_per_epoch=stepsepochs,
                               epochs= epochs, callbacks= model_call_back, validation_data= data_testing_generator,
                               validation_steps= val)
-predictions= model.predict_classes(data_testing_generator)
-confusion_matrix(data_training_generator,predictions)
-print(classification_report(data_training_generator,predictions))
+#predictions= model.predict_classes(data_testing_generator,verbose=1)
+#confusion_matrix(data_training_generator,predictions)
+#print(classification_report(data_training_generator,predictions))
+#model.save_weights('/home/chime/PycharmProjects/Testthesis/Emotions.h5')
--- test.py
+++ test.py
@@ -8,14 +8,18 @@ cascade_classifier = cv2.CascadeClassifier('/home/chime/PycharmProjects/Testthes
 ## defined a model saved from the training session
 model = load_model('/home/chime/PycharmProjects/Testthesis/Emotions.h5')
 ## defining the labels of emotions available
-classes = ['anger', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprised']
+classes = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprised']
+row = 48
+col = 48
 # initilaising camera(laptop camera= 0, external=1)
-#cap = cv2.VideoCapture(0)
-#cap = cv2.VideoCapture('/home/chime/PycharmProjects/Testthesis/frankie2.mp4')
-cap = cv2.VideoCapture('/home/chime/PycharmProjects/Testthesis/clinton.mp4')
+cap = cv2.VideoCapture(0) ##comment this if you want to upload an already existing video
+#cap = cv2.VideoCapture('/home/chime/PycharmProjects/Testthesis/frankie2.mp4') #uncomment and change the path if you want to use this video
+#cap = cv2.VideoCapture('/home/chime/PycharmProjects/Testthesis/clinton.mp4') #uncomment and change the path if you want to use this video
 while True:
     ## return the frame being seen by the camera
     ret, face = cap.read()
+    ##Emotions vector and mapping. the labels are initialised based on how they were arranged in the classes above
+    ## please do not change the arrangement without changing the arrangement above
     labels = []
     angry_0 = []
     disgust_1 = []
@@ -27,13 +31,13 @@ while True:
     ##convert bgr to gray for easy comparism with model
     bgr2gray = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
     ##Reduce the frame of the of the image and detect features
-    face_rects = cascade_classifier.detectMultiScale(bgr2gray,1.3,5)
+    face_rects = cascade_classifier.detectMultiScale(bgr2gray, 1.2, 6)
 
     for (x,y,w,h) in face_rects:
-        ##white solid rectangle for facemapping and thickness of 10
-        cv2.rectangle(face, (x, y), (x+w, y+h), (255, 255, 255), 2)
+        ##white solid rectangle for facemapping and thickness of 5
+        cv2.rectangle(face, (x, y), (x+w, y+h), (255, 255, 255), 5)
         gray_roi = bgr2gray[y:y+h, x:x+w]
-        gray_roi = cv2.resize(gray_roi, (48, 48), interpolation=cv2.INTER_AREA)
+        gray_roi = cv2.resize(gray_roi, (row, col), interpolation=cv2.INTER_AREA)
 
 
 
@@ -42,7 +46,7 @@ while True:
             roi = img_to_array(roi)
             roi = np.expand_dims(roi, axis=0)
 
-        # fetch from the classes and predict the microexpression
+        # fetch from the classes and predict the microexpressions
 
             prediction = model.predict(roi)[0]
             angry_0.append(prediction[0].astype(float))
@@ -54,28 +58,31 @@ while True:
             surprise_6.append(prediction[6].astype(float))
             microexpression = classes[prediction.argmax()]
             label_position = (x,y)
+            ##Position of the micro-expressions present
             i=0
-            ##Text color and type for the microexpression
+            ##Text color and type for the major microexpression
             cv2.putText(face, microexpression, label_position, cv2.FONT_HERSHEY_COMPLEX_SMALL, 2.5, (0, 0, 255),2)
+            ##Text color, type, position of major and minor micro-expressions present
+            ##This is to position all the major and minor micro-expressions at 180 of the screen
             cv2.putText(face, "Angry : " + str(np.round(prediction[0]*100, 2)) + "%", (40, 140 + 180*i),
-                        cv2.FONT_HERSHEY_COMPLEX, 0.5, 255, 1)
+                        cv2.FONT_HERSHEY_COMPLEX, 0.6, 255, 1)
             cv2.putText(face, "Disgust : " + str(np.round(prediction[1]*100, 2)) + "%", (40, 160 + 180*i ),
-                        cv2.FONT_HERSHEY_COMPLEX, 0.5, 255, 1)
+                        cv2.FONT_HERSHEY_COMPLEX, 0.6, 255, 1)
             cv2.putText(face, "Fear : " + str(np.round(prediction[2]*100, 2)) + "%", (40,180 + 180*i ),
-                        cv2.FONT_HERSHEY_COMPLEX, 0.5, 255, 1)
+                        cv2.FONT_HERSHEY_COMPLEX, 0.6, 255, 1)
             cv2.putText(face, "Happy : " + str(np.round(prediction[3]*100, 2)) + "%", (40, 200 + 180 *i),
-                        cv2.FONT_HERSHEY_COMPLEX, 0.5, 255, 1)
+                        cv2.FONT_HERSHEY_COMPLEX, 0.6, 255, 1)
             cv2.putText(face, "Neutral : " + str(np.round(prediction[4]*100, 2)) + "%", (40, 220 + 180 *i),
-                        cv2.FONT_HERSHEY_COMPLEX, 0.5, 255, 1)
+                        cv2.FONT_HERSHEY_COMPLEX, 0.6, 255, 1)
             cv2.putText(face, "Sad : " + str(np.round(prediction[5]*100, 2)) + "%", (40, 240 + 180 *i),
-                        cv2.FONT_HERSHEY_COMPLEX, 0.5, 255, 1)
+                        cv2.FONT_HERSHEY_COMPLEX, 0.6, 255, 1)
             cv2.putText(face, "Surprise : " + str(np.round(prediction[6]*100, 2)) + "%", (40, 260 + 180 *i),
-                        cv2.FONT_HERSHEY_COMPLEX, 0.5, 255, 1)
+                        cv2.FONT_HERSHEY_COMPLEX, 0.6, 255, 1)
 
 
         else:
             ##Text color and type if no face to detect microexpression
-            cv2.putText(face, 'No Face Found', (20, 60), cv2.FONT_HERSHEY_COMPLEX_SMALL, 3, (0, 255, 0), 2)
+            cv2.putText(face, 'No Face Found', (20, 60), cv2.FONT_HERSHEY_COMPLEX_SMALL, 3, (0, 0, 255), 2)
     cv2.imshow('Micro-expression', face)
     ##quit when q is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
